5476e39c4595be56135e7c86073f3630f84d300a
Aesha04 <110104348+Aesha04@users.noreply.github.com>
2024-07-17 10:39:45 +0530

    Add files via upload

---
+++ PRODIGY_WD_03/tic-tac-toe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './components/tic_tac_toe/TicTacToe';
+
+function App() {
+  return (
+    <div >
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
